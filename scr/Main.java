b07d67321324d05d5db20c822693efd8288ceca6
Christian <118530836+faze-chris@users.noreply.github.com>
2025-09-11 15:27:52 +0200

    HBO-ICT | lesson 1 done

--- scr/Main.java
+++ scr/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+//        System.out.println("Hello, world!");
+        Scanner scanner = new Scanner(System.in);
+        int getal1 = scanner.nextInt();
+        int getal2 = scanner.nextInt();
+        scanner.nextLine();
+        String operator = scanner.nextLine();
+
+        System.out.println(getal2);
+        System.out.println(operator);
+        System.out.println(getal2);
+        System.out.println("=");
+        System.out.println(getal1 + getal2);
     }
 }
 
