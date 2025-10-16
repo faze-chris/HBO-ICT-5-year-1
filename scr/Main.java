82d2ab9af5a20a52d749cd7f4df93925362bee63
Christian <118530836+faze-chris@users.noreply.github.com>
2025-10-16 13:25:27 +0200

    Update Main.java

--- scr/Main.java
+++ scr/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Scanner scanner = new Scanner(System.in);
+        int getal = scanner.nextInt();
+        scanner.nextLine();
+        String letter = scanner.nextLine();
+
+        if (letter.equals("N")) {
+            System.out.println("Heb je een rijbewijs?");
+            System.out.println("Mee naar het bureau!");
+        }
+        else if (letter.equals("J") && getal > 2) {
+            System.out.println("Heb je een rijbewijs?");
+            System.out.println("Hoeveel biertjes heb je gedronken?");
+            System.out.println("Rijbewijs inleveren!");
+        }
+        else if (letter.equals("J")) {
+            System.out.println("Heb je een rijbewijs?");
+            System.out.println("Hoeveel biertjes heb je gedronken?");
+            System.out.println("Een goede avond verder!");
+        }
+
     }
 }
-
