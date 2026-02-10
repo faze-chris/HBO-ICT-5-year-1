5db7c5844e9faf2e757c5060f338ba32a68068f6
Christian <118530836+faze-chris@users.noreply.github.com>
2026-02-10 09:31:21 +0100

    sfsfds

--- scr/Main.java
+++ scr/Main.java
@@ -1,6 +1,73 @@
 public class Main {
+
+    private static void machtsverheffen() {
+
+    }
+
+    private static void tafelBlad() {
+
+    }
+
+    private static void galgje() {
+        String[][] woorden =
+                {
+                    {"o","c","h","t","e","n","d"},
+                    {"v","o","e","t","b","a","l"},
+                    {"k","n","o","p","p","e","n"},
+                    {"t","a","f","e","l","e","n"},
+                    {"f","i","e","t","s","e","n"},
+                    {"b","l","o","e","m","e","n"},
+                    {"s","l","a","p","e","n","d"},
+                    {"w","e","r","k","l","u","i"},
+                    {"z","o","n","l","o","o","s"},
+                    {"g","e","l","a","t","e","n"},
+                    {"l","o","p","e","n","d","e"},
+                    {"f","l","e","s","s","e","n"},
+                    {"d","r","i","n","k","e","n"},
+                    {"l","a","c","h","e","n","d"},
+                    {"d","r","o","m","e","n","d"},
+                    {"p","r","a","t","e","n","d"},
+                    {"g","e","z","e","g","d","e"},
+                    {"p","l","a","n","t","e","n"},
+                    {"s","t","o","e","l","e","n"},
+                    {"m","a","r","k","t","e","n"},
+                    {"z","o","n","d","a","g","s"},
+                    {"w","i","n","t","e","r","s"},
+                    {"z","o","m","e","r","s","e"},
+                    {"a","p","p","e","l","e","n"},
+                    {"k","e","u","k","e","n","s"}
+                };
+    }
+
+    private static void mop() {
+
+    }
+
+    private static void printHoofdMenu() {
+
+    }
+
+    private static void printWiskundeMenu() {
+
+    }
+
+    private static void printPlezierMenu() {
+
+    }
+
+    public static void menuWiskunde() {
+
+    }
+
+    public static void menuPlezier(){
+
+    }
+
+    private static void hoofdMenu() {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hoofdMenu();
     }
 }
-
