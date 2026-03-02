bceec0a42f87288acf251ae000a8624c7ae471ed
Christian <118530836+faze-chris@users.noreply.github.com>
2026-03-02 12:20:43 +0100

    save

--- scr/Main.java
+++ scr/Main.java
@@ -7,13 +7,13 @@ public class Main {
         // methodes calls
 //        autoVanArthure.rijden();
 //        autoVanArthure.starten();
-        autoVanArthure.status();
+//        autoVanArthure.status();
         // filling atributes
 //      autoVanArthure.deur = 2;
 //      autoVanArthure.kleur = "Blauw" ;
 //      autoVanArthure.wielen = 4 ;
         //printing atrui buts
-//        System.out.println(autoVanArthure.deur);
+//        System.out.println(autoVanArthure.wielen);
 //        System.out.println(autoVanArthure.kleur);
 //        System.out.println(autoVanArthure.wielen);
 
