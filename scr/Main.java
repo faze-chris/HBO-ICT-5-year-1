e8c159e6b97e056930166d82bdf64dab0fd92967
Christian <118530836+faze-chris@users.noreply.github.com>
2026-03-02 09:57:03 +0100

    save

--- scr/Main.java
+++ scr/Main.java
@@ -1,6 +1,19 @@
+//import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+//        Product gucciTas = Product.Tas;
+//        System.out.println(gucciTas.prijs);
+
+
+//        HashMap<String, Double> producten = new HashMap<>();
+//        producten.put(key:"tas", value: 5);
+//        producten.put(key:"Jas", value: 15);
+//        producten.put(key:"Schoenen", value: 20);
+//        System.out.println(producten.get(key: "Jas"));
+
+        
+
     }
 }
 
---
+++ scr/Product.java
@@ -0,0 +1,10 @@
+public enum Product {
+    Tas(45),
+    Schoenen(45),
+    Jas(45);
+
+    int prijs;
+    Product(int prijs){
+        this.prijs = prijs;
+    }
+}
