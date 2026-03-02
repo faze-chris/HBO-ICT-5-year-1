5346a6e35e4ec45cef81994db521d321f7f919b5
Christian <118530836+faze-chris@users.noreply.github.com>
2026-03-02 09:35:43 +0100

    save

--- scr/Vakje.java
+++ scr/Vakje.java
@@ -1,4 +1,26 @@
 public class Vakje {
-    int LocatieX;
-    int LocatieY;
+    private int locatieX;
+    private int locatieY;
+
+    private  Bolletje bolletje;
+    private Superbol superbol;
+
+    private Bolletje bolletje;
+    private Superbol superbol;
+
+    public Vakje(int locatieX, int locatieY) {
+        this.locatieX = locatieX;
+        this.locatieY = locatieY;
+    }
+
+    public void setBolletje(Bolletje bolletje) {
+        this.bolletje = bolletje;
+    }
+
+    public Bolletje getBolletje() {
+        return bolletje;
+    }
+
+
 }
+
