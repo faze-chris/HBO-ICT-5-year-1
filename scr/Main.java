945622c2b1f8132cc1800b7686176b1586c501f3
Christian <118530836+faze-chris@users.noreply.github.com>
2026-03-02 11:48:53 +0100

    save

--- scr/Main.java
+++ scr/Main.java
@@ -1,16 +1,16 @@
-//import java.util.HashMap;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-//        Product gucciTas = Product.Tas;
-//        System.out.println(gucciTas.prijs);
+        Product gucciTas = Product.Tas;
+        System.out.println(gucciTas.prijs);
 
 
-//        HashMap<String, Double> producten = new HashMap<>();
-//        producten.put(key:"tas", value: 5);
-//        producten.put(key:"Jas", value: 15);
-//        producten.put(key:"Schoenen", value: 20);
-//        System.out.println(producten.get(key: "Jas"));
+        HashMap<String, Integer> producten = new HashMap<>();
+        producten.put("tas",  5);
+        producten.put("Jas", 15);
+        producten.put("Schoenen", 20);
+        System.out.println(producten.get("Jas"));
 
         
 
