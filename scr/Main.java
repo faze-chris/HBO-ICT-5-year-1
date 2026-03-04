cf2634946fe576e17aeaa962e8dde4ef8a9cf881
Christian <118530836+faze-chris@users.noreply.github.com>
2026-03-04 12:20:44 +0100

    save

--- scr/Main.java
+++ scr/Main.java
@@ -1,6 +1,7 @@
-public class Main {
-    public static void main(String[] args)   {
-        System.out.println("Hello, world!");
+    public class Main {
+        public static void main(String[] args)   {
+            Game myGame = new Game(false, false);
+            myGame.startGame();
+        }
     }
-}
 
